5bc0ef8f6128c0eda3d4c810d1bc7386ba638ecc
Mehdi <marzban2000@gmail.com>
2020-01-17 14:51:56 -0700

    finished yes Changes to be committed:

--- ENSF409_Lab00/src/Converter.java
+++ ENSF409_Lab00/src/Converter.java
@@ -18,6 +18,8 @@ public class Converter {
 	public static void main(String[] args) {
 //TODO: The first student will implement this method.
 // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
-// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value
+		double x= celsiusToFahrenheit(180);
+		double y= fahrenheitToCelsius(250);
 	}
 }
