f6303f344cdf95b229d20873807697fd78165d4f
b4yc <cbaylee123@gmail.com>
2020-01-22 15:18:58 -0700

    added km to miles

--- ENSF409_Lab00/src/Converter.java
+++ ENSF409_Lab00/src/Converter.java
@@ -15,6 +15,11 @@ public class Converter {
  // TODO: The second student will implement this method
 		return (F-32)*5/9;
 	}
+
+	private double kilometersToMiles(double K){
+		return (K/1.609);
+	}
+	
 	public static void main(String[] args) {
 //TODO: The first student will implement this method.
 // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
@@ -23,3 +28,4 @@ public class Converter {
 		double y= fahrenheitToCelsius(250);
 	}
 }
+
