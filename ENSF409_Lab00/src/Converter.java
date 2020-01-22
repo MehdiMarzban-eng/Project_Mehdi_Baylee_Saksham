9fc948dce3df05c1a9e1282940491302b49f3007
Saksham98123 <44077114+Saksham98123@users.noreply.github.com>
2020-01-22 15:24:39 -0700

    Issue Fixed

--- ENSF409_Lab00/src/Converter.java
+++ ENSF409_Lab00/src/Converter.java
@@ -1,4 +1,3 @@
-
 public class Converter {
 //Your names go here:
 /*
@@ -9,13 +8,12 @@ public class Converter {
 */
 	private double celsiusToFahrenheit(double C){
 // TODO: The third student will implement this method
-		return ((C*(9/5))+32);
+		return ((C*9/5)+32);
 	}
 	private double fahrenheitToCelsius(double F){
  // TODO: The second student will implement this method
 		return (F-32)*5/9;
 	}
-
 	private double kilometersToMiles(double K){
 		return (K/1.609);
 	}
@@ -24,8 +22,14 @@ public class Converter {
 //TODO: The first student will implement this method.
 // Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
 // Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value
-		double x= celsiusToFahrenheit(180);
-		double y= fahrenheitToCelsius(250);
+		Converter x= new Converter();
+		
+		Converter y= new Converter();
+		double f = x.celsiusToFahrenheit(180);
+		
+		double c= y.fahrenheitToCelsius(250);
+		System.out.println("Temp in Fahrenheit = "+f);
+		System.out.println("Temp in Celsius = "+c);
 	}
+	
 }
-
